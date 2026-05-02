814c20c29d4c7467d464c3dbd5dfc0a0bb7f8392
IT2410291 <it24102971@my.sliit.lk>
2026-05-02 10:08:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import app from "./src/app.js";
 import { connectDB } from "./src/config/db.js";
+import dns from "dns";
+// ... other imports
 
+// DNS Configuration
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const PORT = process.env.PORT || 5000;
 
 connectDB()
